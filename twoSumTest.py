6fdab9fae30896f8ee2d54159af8ef71600790b0
ray_chu <raymondzchu@gmail.com>
2024-04-24 21:51:50 -0400

    Added two-sum

--- twoSumTest.py
+++ twoSumTest.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         # Create a dictionary to store the elements and their indices
         num_dict = {}
         
